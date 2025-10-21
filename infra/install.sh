a5f25734eba06ad1191ab9f69d9c03d6398e3ff3
Tran Duy Truong <harunaga113@gmail.com>
2025-10-21 21:43:02 +0700

    chore: init

---
+++ infra/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Starting installation process..."
+
+# Install necessary dependencies
+echo "[1] Installing kind..."
+
+# Install kind
+[ $(uname -m) = arm64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-darwin-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+echo "[2] Installing kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/arm64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo "[3] Install completed."
